504a2c01f48626ba09a55f59dc894bd7682bc9cc
dinhvantruyen <dinhvantruyen271006@gmail.com>
2026-01-28 10:09:14 +0700

    fix: bind to process.env.PORT

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 const app = require("./app");
 const { connectDB } = require("./config/db");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+       console.log("Server running on port", PORT);
     });
   })
   .catch((err) => {
